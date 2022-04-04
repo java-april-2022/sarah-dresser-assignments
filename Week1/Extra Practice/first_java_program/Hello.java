65143f8d1da94520efc8dc91611e7752b548bec4
sdress <sarah.dresser1@gmail.com>
2022-04-04 10:34:44 -0700

    creating first Java program, practice assignment

---
+++ Week1/Extra Practice/first_java_program/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    // print statement:
+    public static void main(String[] args) {
+        System.out.println("My name is Sarah");
+        System.out.println("I am 26 years old");
+        System.out.println("My hometown is Diamond Bar, CA");
+    }
+}
