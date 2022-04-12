a0c69cbb4be778afbede6d8c1acc9b5d39a045a1
sdress <sarah.dresser1@gmail.com>
2022-04-11 17:49:54 -0700

    updating classes

--- Week2/Extra Practice/Barista_Challenge/Item.java
+++ Week2/Extra Practice/Barista_Challenge/Item.java
@@ -1,8 +1,31 @@
+
 public class Item {
     // private member variables
     private String name;
     private double price;
 
-    // constructor:
-    
+    // constructor w/ name & price as arguments:
+    public Item(String name, double price){
+        // pointing to this w/ parameters
+        this.name = name;
+        this.price = price;
+    }
+
+    //setters and getters:
+    public String getName(){
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
 }
--- Week2/Extra Practice/Barista_Challenge/Order.java
+++ Week2/Extra Practice/Barista_Challenge/Order.java
@@ -1,3 +1,20 @@
+import java.util.ArrayList;
+
 public class Order {
     // private member variables
+    private String name;
+    private boolean ready;
+    private ArrayList<Item> items;
+
+    // constructor w/ no params, sets name = "Guest"
+    public Order(){
+        this.name = "Guest";
+        this.items = new ArrayList<Item>();
+    }
+
+    // overloaded constructor w/ name param
+    public Order(String name){
+        this.name = name;
+        this.items = new ArrayList<Item>();
+    }
 }
