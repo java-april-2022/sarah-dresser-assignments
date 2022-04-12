43daa42c5ec8bdfcbdba7a892c399f6f4c154eb5
sdress <sarah.dresser1@gmail.com>
2022-04-11 17:53:39 -0700

    adding order methods

--- Week2/Extra Practice/Barista_Challenge/Order.java
+++ Week2/Extra Practice/Barista_Challenge/Order.java
@@ -10,11 +10,41 @@ public class Order {
     public Order(){
         this.name = "Guest";
         this.items = new ArrayList<Item>();
+        this.ready = false;
     }
 
     // overloaded constructor w/ name param
     public Order(String name){
         this.name = name;
         this.items = new ArrayList<Item>();
+        this.ready = false;
     }
+
+    // getters(accessors) and setters(mutators)
+    // getters:
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Item> getItems() {
+        return items;
+    }
+
+    public boolean getReady(){
+        return ready;
+    }
+
+    // setters:
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setItems(ArrayList<Item> items) {
+        this.items = items;
+    }
+
+    public void setReady(boolean ready) {
+        this.ready = ready;
+    }
+    
 }
