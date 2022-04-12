e2b5dfd6ddc3dc19e773e115b32633fb35e2413c
sdress <sarah.dresser1@gmail.com>
2022-04-12 16:17:48 -0700

    cleaning up test file

--- Week2/Bank Account/TestBankAccount.java
+++ Week2/Bank Account/TestBankAccount.java
@@ -9,27 +9,22 @@ public class TestBankAccount {
         // print tests:
 
         // check Account1
-        System.out.printf("\nAccount 1 Checking balance: $%.2f", account1.getCheckingBalance());
-        System.out.printf("\nAccount 1 Savings balance: $%.2f", account1.getSavingsBalance());
+        System.out.printf("\nAccount 1 Checking balance: $%.2f, Savings balance: $%.2f", account1.getCheckingBalance(), account1.getSavingsBalance());
         
         // check Account2
-        System.out.printf("\nAccount 2 Checking balance: $%.2f", account2.getCheckingBalance());
-        System.out.printf("\nAccount 2 Savings balance: $%.2f", account2.getSavingsBalance());
+        System.out.printf("\nAccount 2 Checking balance: $%.2f, Savings balance: $%.2f", account2.getCheckingBalance(), account2.getSavingsBalance());
         
         // check deposit and withdrawal methods
         account2.withdraw("Checking", 8800);
-        // System.out.printf("\nNew checking balance: $%.2f", account2.getCheckingBalance());
-
         account1.deposit("savings", 500);
+
         // check over-withdrawal
         account1.withdraw("Checking", 500);
-        // System.out.printf("\n");
 
         // check totalAmount method
         System.out.printf("\nAccount 1 Total Amount: %.2f", account1.getAccountTotal());
         System.out.printf("\nAccount 2 Total Amount: %.2f", account2.getAccountTotal());
 
-        
         // check for NumAccounts & AllAccountsBalance
         System.out.printf("\nNumber of Accounts: %d", BankAccount.getNumAccounts());
         System.out.printf("\nTotal Balance of Accounts: %.2f", BankAccount.getAllAccountsBalance());
