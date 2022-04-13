f43568ac08d676276b06d9073566adf44444ee67
sdress <sarah.dresser1@gmail.com>
2022-04-13 16:28:47 -0700

    adding completed Zoo Keeper Part 1 practice assignment

---
+++ Week2/Zoo Keeper Part 2/Zoo Keeper Part 1/src/GorillaTest.java
@@ -0,0 +1,16 @@
+
+public class GorillaTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Gorilla kingKong = new Gorilla();
+		kingKong.throwSomething();
+		kingKong.eatBananas();
+		kingKong.throwSomething();
+		kingKong.climb();
+		kingKong.eatBananas();
+		kingKong.throwSomething();
+
+	}
+
+}
---
+++ Week2/Zoo Keeper Part 2/Zoo Keeper Part 1/src/Mammal.java
@@ -0,0 +1,9 @@
+
+public class Mammal {
+	protected int energyLevel = 50;
+	
+	public int displayEnergy() {
+		System.out.printf("Energy Level: %d", energyLevel);
+		return energyLevel;
+	}
+}
