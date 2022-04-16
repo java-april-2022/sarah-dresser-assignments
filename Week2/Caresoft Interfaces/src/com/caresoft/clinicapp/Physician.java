93e75e845e7a073b3741f9c176389c7b292956cb
sdress <sarah.dresser1@gmail.com>
2022-04-15 21:47:03 -0700

    updating Physician class

--- Week2/Caresoft Interfaces/src/com/caresoft/clinicapp/Physician.java
+++ Week2/Caresoft Interfaces/src/com/caresoft/clinicapp/Physician.java
@@ -1,11 +1,24 @@
 package com.caresoft.clinicapp;
 
-public class Physician extends User implements HIPAACompliantUser {
+public class Physician extends User implements HIPAACompliantUser {  
+    private ArrayList<String> patientNotes;
+	
+    // TO DO: Constructor that takes an ID
+    // TO DO: Implement HIPAACompliantUser!
+    
+    public Physician(Integer id) {
+    	super(id);
+    }
 
-	public Physician() {
-		// TODO Auto-generated constructor stub
-	}
+	public void newPatientNotes(String notes, String patientName, Date date) {
+        String report = String.format(
+            "Datetime Submitted: %s \n", date);
+        report += String.format("Reported By ID: %s\n", this.id);
+        report += String.format("Patient Name: %s\n", patientName);
+        report += String.format("Notes: %s \n", notes);
+        this.patientNotes.add(report);
+    }
 	
-	
+    // TO DO: Setters & Getters
 
 }
