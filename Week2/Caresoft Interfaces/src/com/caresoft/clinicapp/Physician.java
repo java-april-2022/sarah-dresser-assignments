d3186ca02a4219491d7c39d43f8adc5a54044ac3
sdress <sarah.dresser1@gmail.com>
2022-04-15 21:26:41 -0700

    adding file structure for Caresoft Interfaces assignment

---
+++ Week2/Caresoft Interfaces/src/com/caresoft/clinicapp/Physician.java
@@ -0,0 +1,11 @@
+package com.caresoft.clinicapp;
+
+public class Physician extends User implements HIPAACompliantUser {
+
+	public Physician() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
