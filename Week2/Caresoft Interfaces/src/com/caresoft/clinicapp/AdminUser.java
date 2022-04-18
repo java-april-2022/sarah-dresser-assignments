e5c30c6331c59a022787edeaf1f61f3ce766ef24
sdress <sarah.dresser1@gmail.com>
2022-04-17 21:05:02 -0700

    updating test/AdminUser class

--- Week2/Caresoft Interfaces/src/com/caresoft/clinicapp/AdminUser.java
+++ Week2/Caresoft Interfaces/src/com/caresoft/clinicapp/AdminUser.java
@@ -1,9 +1,71 @@
 package com.caresoft.clinicapp;
 
-public class AdminUser extends User implements HIPAACompliantUser, HIPAACompliantAdmin {
+import java.util.ArrayList;
+import java.util.Date;
 
-	public AdminUser() {
-		// TODO Auto-generated constructor stub
+public class AdminUser extends User implements HIPAACompliantUser, HIPAACompliantAdmin {
+    
+    private String role;
+    private ArrayList<String> securityIncidents;
+    
+    public AdminUser(Integer id, String role) {
+    	super(id);
+    	this.setRole(role);
+    }
+    // TO DO: Implement a constructor that takes an ID and a role
+    // TO DO: Implement HIPAACompliantUser!
+    // TO DO: Implement HIPAACompliantAdmin!
+    
+    @Override
+    public boolean assignPin(int pin) {
+    	// TODO Auto-generated method stub
+    	if( String.valueOf(pin).length() < 6 ) {
+    		return false;
+    	}
+    	else {
+    		return true;
+    	}
+    }
+    @Override
+    public boolean accessAuthorized(Integer confirmedAuthID) {
+    	// TODO Auto-generated method stub
+    	if( confirmedAuthID == this.id ) {
+			return true;
+		}
+		else {
+			return false;
+		}
+    }
+    @Override
+    public ArrayList<String> reportSecurityIncidents() {
+    	// TODO Auto-generated method stub
+    	return null;
+    }
+    
+    public void newIncident(String notes) {
+        String report = String.format(
+            "Datetime Submitted: %s \n,  Reported By ID: %s\n Notes: %s \n", 
+            new Date(), this.id, notes
+        );
+        securityIncidents.add(report);
+    }
+    public void authIncident() {
+        String report = String.format(
+            "Datetime Submitted: %s \n,  ID: %s\n Notes: %s \n", 
+            new Date(), this.id, "AUTHORIZATION ATTEMPT FAILED FOR THIS USER"
+        );
+        securityIncidents.add(report);
+    }
+
+    // TO DO: Setters & Getters
+	public String getRole() {
+		return role;
 	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+    
+	
 
 }
