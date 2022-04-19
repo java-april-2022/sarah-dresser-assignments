cac372d5e122af4981d26c6e0746ec1e1adf9a98
sdress <sarah.dresser1@gmail.com>
2022-04-19 15:59:40 -0700

    adding first Spring Boot project!

---
+++ Week3/Extra Practice/firstproject/src/test/java/com/sarah/firstproject/FirstprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarah.firstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
