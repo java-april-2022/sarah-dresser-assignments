02cac7c3c1630ad89fb99a5afed80af6e898b320
sdress <sarah.dresser1@gmail.com>
2022-04-19 16:17:49 -0700

    updating HomeController file

--- Week3/Extra Practice/controllerspractice/src/main/java/com/sarah/controllerspractice/HomeController.java
+++ Week3/Extra Practice/controllerspractice/src/main/java/com/sarah/controllerspractice/HomeController.java
@@ -4,9 +4,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/home")
 public class HomeController {
-	@RequestMapping("/")
+	@RequestMapping("")
 	public String hello() {
 		return "Hello World";
+	}
+	@RequestMapping("/world")
+	public String world() {
+		return "Class level annotations are cool";
 	}
 }
