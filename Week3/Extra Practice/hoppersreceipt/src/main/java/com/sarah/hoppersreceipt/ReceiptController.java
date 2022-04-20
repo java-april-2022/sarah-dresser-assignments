6fb56dc59c5135ba86c80cc44291c84c6c17c054
sdress <sarah.dresser1@gmail.com>
2022-04-20 12:53:56 -0700

    adding MVC structure files

---
+++ Week3/Extra Practice/hoppersreceipt/src/main/java/com/sarah/hoppersreceipt/ReceiptController.java
@@ -0,0 +1,23 @@
+package com.sarah.hoppersreceipt;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ReceiptController {
+	@RequestMapping("/")
+	public String index(Model model) {
+		
+		// given variables
+		String name = "Grace Hopper";
+		String itemName = "Copper wire";
+		double price = 5.25;
+		String description = "Metal strips, also an illustration of nanoseconds.";
+		String vendor = "Little Things Corner Store";
+		
+		
+		
+		return "index.jsp";
+	}
+}
