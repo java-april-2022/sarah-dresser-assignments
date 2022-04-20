0f18bfb27f295074563da6b7ae490e7490375fd7
sdress <sarah.dresser1@gmail.com>
2022-04-19 21:58:38 -0700

    adding completed Daikichi Path Variables assignment

--- Week3/Daikichi Path Variables/daikichiroutes/src/main/java/com/sarah/daikichiroutes/DaikichiController.java
+++ Week3/Daikichi Path Variables/daikichiroutes/src/main/java/com/sarah/daikichiroutes/DaikichiController.java
@@ -20,8 +20,18 @@ public class DaikichiController {
 		return "Tomorrow an opportunity will arise, so be sure to be open to new ideas!";
 	}
 	// adding @PathVariable routes
+	// travel() returns string w/ city variable
 	@RequestMapping("/travel/{city}")
 	public String travel(@PathVariable("city") String city) {
 		return String.format("Congratulations! You will soon travel to %s!", city);
+	}
+	// lotto() returns different daikichi if num is even or odd
+	@RequestMapping("/lotto/{num}")
+	public String lotto(@PathVariable("num") int num) {
+		if (num%2==0) {
+			return "You will take a grand journey in the near future, but be weary of tempting offers";
+		} else {
+			return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+		}
 	}
 }
