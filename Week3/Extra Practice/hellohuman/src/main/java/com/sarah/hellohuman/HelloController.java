7ea35f7cd56f7e0de1bf87db70e64fed38294258
sdress <sarah.dresser1@gmail.com>
2022-04-19 18:21:00 -0700

    adding ninja bonus

--- Week3/Extra Practice/hellohuman/src/main/java/com/sarah/hellohuman/HelloController.java
+++ Week3/Extra Practice/hellohuman/src/main/java/com/sarah/hellohuman/HelloController.java
@@ -6,13 +6,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-	// index() returns "Hello Human" if no name provided, else "Hello Name"
-	@RequestMapping("/")
-	public String index(@RequestParam(value = "q", required=false) String name) {
-	if(name == null) {
-		return "Hello Human";
+	// returns "Hello Human" if no name provided
+	// returns "Hello first_name" if only first name provided
+	// returns "Hello full_name" if both first & last name provided
+	@RequestMapping("")
+	public String giveName(
+			@RequestParam(value = "first_name", required=false) String first_name,
+			@RequestParam(value = "last_name", required=false) String last_name
+			) {
+		
+	String full_name;
+		
+	if(first_name != null && last_name == null) {
+		full_name = first_name;
+	} else if (first_name != null && last_name != null) {
+		full_name = first_name + " " + last_name;
 	} else {
-		return "Hello " + name;	
+		full_name = "Human";
 	}
+	return "Hello " + full_name;
 	}
 }
