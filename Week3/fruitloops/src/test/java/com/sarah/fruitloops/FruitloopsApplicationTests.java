991999e01a8bb25ee1610cc6702d961dcc7f686b
sdress <sarah.dresser1@gmail.com>
2022-04-20 17:30:32 -0700

    setting up project files

---
+++ Week3/fruitloops/src/test/java/com/sarah/fruitloops/FruitloopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarah.fruitloops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitloopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
