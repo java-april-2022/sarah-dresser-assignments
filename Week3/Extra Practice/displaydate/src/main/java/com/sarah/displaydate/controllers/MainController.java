df140a9f83f3775f1edd0c1ad5a787379e4d401b
sdress <sarah.dresser1@gmail.com>
2022-04-23 11:18:50 -0700

    adding date model

--- Week3/Extra Practice/displaydate/src/main/java/com/sarah/displaydate/controllers/MainController.java
+++ Week3/Extra Practice/displaydate/src/main/java/com/sarah/displaydate/controllers/MainController.java
@@ -1,5 +1,27 @@
 package com.sarah.displaydate.controllers;
 
-import java.util.Date();
+import java.util.Date;
 
-import 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping("/")
+	public String index() {
+		return "index.jsp";
+	}
+	
+	@RequestMapping("/date")
+	public String getDate(Model model) {
+		
+		return "date.jsp";
+	}
+	
+	@RequestMapping("/time")
+	public String getTime() {
+		return "time.jsp";
+	}
+}
--- Week3/Extra Practice/displaydate/src/main/java/com/sarah/displaydate/models/Date.java
+++ Week3/Extra Practice/displaydate/src/main/java/com/sarah/displaydate/models/Date.java
@@ -1,5 +1,41 @@
 package com.sarah.displaydate.models;
 
 public class Date {
+	// MEMBER VARIABLES
+	private String month;
+	private int day;
+	private int year;
+	
+	// CONSTRUCTOR
+	public Date(String month, int day, int year) {
+		this.month = month;
+		this.day = day;
+		this.year = year;
+	}
+
+	// GETTERS & SETTERS
+	public String getMonth() {
+		return month;
+	}
+
+	public void setMonth(String month) {
+		this.month = month;
+	}
+
+	public int getDay() {
+		return day;
+	}
+
+	public void setDay(int day) {
+		this.day = day;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
 
 }
