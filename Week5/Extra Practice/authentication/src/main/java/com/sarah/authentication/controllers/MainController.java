8889bba7145d7d856d968ae561d8ba268a4651a0
sdress <sarah.dresser1@gmail.com>
2022-05-04 17:02:56 -0700

    updating project

--- Week5/Extra Practice/authentication/src/main/java/com/sarah/authentication/controllers/MainController.java
+++ Week5/Extra Practice/authentication/src/main/java/com/sarah/authentication/controllers/MainController.java
@@ -1,13 +1,62 @@
 package com.sarah.authentication.controllers;
 
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.sarah.authentication.models.LoginUser;
+import com.sarah.authentication.models.User;
 
 @Controller
 public class MainController {
 	
+	/*
+	 * @Autowired private UserService userService;
+	 */
+	
 	@GetMapping("/")
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("newUser", new User());
+		model.addAttribute("newLogin", new LoginUser());
 		return "index.jsp";
+	}
+	
+	@PostMapping("/register")
+	public String create(@Valid @ModelAttribute("newUser") User newUser, BindingResult results, Model model, HttpSession session) {
+		// Add: register method in UserService
+		// that will validate & create User
+		if(results.hasErrors()) {
+			// need to send empty LoginUser before
+			// re-rendering page
+			model.addAttribute("newLogin", new LoginUser());
+			return "index.jsp";
+		}
+		// Add: store user ID in session
+		return "redirect:/welcome";
+	}
+	
+	@PostMapping("/login")
+	public String login(@Valid @ModelAttribute("newLogin") LoginUser newLogin, BindingResult results, Model model, HttpSession session) {
+		// once service is implemented:
+		// User user = userService.login(newLogin, result);
+		if(results.hasErrors()) {
+			model.addAttribute("newUser", new User());
+			return "index.jsp";
+		}
+		
+		// Add: store ID from DB in session
+		// and log them in
+		return "redirect:/welcome";
+	}
+	
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "dashboard.jsp";
 	}
 }
--- Week5/Extra Practice/authentication/src/main/java/com/sarah/authentication/services/UserService.java
+++ Week5/Extra Practice/authentication/src/main/java/com/sarah/authentication/services/UserService.java
@@ -1,8 +1,57 @@
 package com.sarah.authentication.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+
+import com.sarah.authentication.models.User;
+import com.sarah.authentication.repositories.UserRepository;
 
 @Service
 public class UserService {
 
+	@Autowired
+	UserRepository repo;
+	
+	// Get all Users
+	public List<User> getAllUsers(){
+		return repo.findAll();
+	}
+	
+	// Register & Login Methods
+	public User register(User newUser, BindingResult result) {
+		
+		// creating instance of potential user
+		Optional<User> potentialUser = repo.findByEmail(newUser.getEmail());
+		
+		// if email is already in database,
+		// reject potential user
+		if(potentialUser.isPresent()) {
+			result.rejectValue("email", "Matches", "An account with that email already exists!");
+		}
+		
+		// reject if password != confirm
+		if(!newUser.getPassword().equals(newUser.getConfirm())) {
+			result.rejectValue("password", "Matches", "Confirm password must match!");
+		}
+		
+		// if errors in form, return null
+		if(result.hasErrors()) {
+			return null;
+		}
+		
+		// if all checks pass, hash password and 
+		// save user to db
+		
+		// hashed pw
+		String hashed = BCrypt.hashpw(newUser.getPassword(), BCrypt.gensalt());
+		// save hashed pw to user
+		newUser.setPassword(hashed);
+		
+		return repo.save(newUser);
+	}
 }
