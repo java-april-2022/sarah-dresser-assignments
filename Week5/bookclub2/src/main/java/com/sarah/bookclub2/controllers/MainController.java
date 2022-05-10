4b10eb83e1b8a869b13022f22cfe0308f3a78738
sdress <sarah.dresser1@gmail.com>
2022-05-10 13:09:30 -0700

    adding Login logic

--- Week5/bookclub2/src/main/java/com/sarah/bookclub2/controllers/MainController.java
+++ Week5/bookclub2/src/main/java/com/sarah/bookclub2/controllers/MainController.java
@@ -1,9 +1,18 @@
 package com.sarah.bookclub2.controllers;
 
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.sarah.bookclub2.models.LoginUser;
+import com.sarah.bookclub2.models.User;
 import com.sarah.bookclub2.services.UserService;
 
 @Controller
@@ -13,7 +22,28 @@ public class MainController {
 	UserService userService;
 	
 	@GetMapping("/")
-	public String index() {
+	public String index(Model model) {
+		// binding empty User & LoginUser objects to save form data
+		model.addAttribute("newUser", new User());
+		model.addAttribute("newLogin", new LoginUser());
 		return "index.jsp";	
+	}
+	
+	@PostMapping("/register")
+	public String register(@Valid @ModelAttribute("newUser") User newUser, BindingResult result, Model model, HttpSession session) {
+		
+		if(result.hasErrors()) {
+			model.addAttribute("newLogin", new LoginUser());
+			return "index.jsp";
+		}
+		
+		// register method from UserService
+		User user = userService.register(newUser, result);
+		
+		// if no errors, continue
+		// storing user ID in session
+		session.setAttribute("userId", user.getId());
+		
+		return "redirect:/books";
 	}
 }
--- Week5/bookclub2/src/main/java/com/sarah/bookclub2/services/UserService.java
+++ Week5/bookclub2/src/main/java/com/sarah/bookclub2/services/UserService.java
@@ -4,10 +4,12 @@ import java.util.Optional;
 
 import javax.servlet.http.HttpSession;
 
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
 
+import com.sarah.bookclub2.models.LoginUser;
 import com.sarah.bookclub2.models.User;
 import com.sarah.bookclub2.repositories.UserRepository;
 
@@ -24,32 +26,75 @@ public class UserService {
 	}
 	
 	// check if user is in session
-	public Optional<User> inSession(Long userId, HttpSession session){
-		session.getAttribute(userId);
+	public boolean inSession(HttpSession session){
+		if(session.getAttribute("userId") == null) {
+			return false;
+		}
+		return true;
 	}
 	
 	// Register and Login Methods
+	// from: https://login.codingdojo.com/m/325/9950/74045
 	
 	public User register(User newUser, BindingResult result) {
 		
 		// creating instance of potential user
-		
+		Optional<User> potentialUser = userRepo.findByEmail(newUser.getEmail());
 		
 		// if email is already in database,
-		// reject potential user
+		// reject user
+		if(potentialUser != null) {
+			System.out.println("From UserService: email already in DB");
+			result.rejectValue("email", "An account with that email already exists");
+		}
 		
 		// reject if password != confirm
+		if(newUser.getPassword() != newUser.getConfirm()) {
+			 System.out.println("From UserService: passwords do not match");
+			 result.rejectValue("confirm", "Passwords must match");
+		}
 		
 		// if errors in form, return null
+		if(result.hasErrors()) {
+			return null;
+		}
 		
 		// if all checks pass, continue
 		
 		// hash pw & save to user
+		String hashed = BCrypt.hashpw(newUser.getPassword(), BCrypt.gensalt());
+		newUser.setPassword(hashed);
 		
 		// saving to repository
+		return userRepo.save(newUser);
 	}
 	
-	public User login() {
+	public User login(LoginUser newLogin, BindingResult result) {
+		// creating instance of potential user
+		Optional<User> potentialUser = userRepo.findByEmail(newLogin.getEmail());
 		
+		// find user in DB by email
+		if(potentialUser == null) {
+			System.out.println("From UserService: email not found");
+			result.rejectValue("email", "Email not found");
+		}
+		
+		// continue
+		// user exists, retrieve from DB
+		User user = potentialUser.get();
+		
+		// reject if BCrypt password does not match
+		if(!BCrypt.checkpw(newLogin.getPassword(), user.getPassword())) {
+			result.rejectValue("password", "Invalid Password--try again!");
+		}
+		
+		// if any errors, return null
+		if(result.hasErrors()) {
+			System.out.println("From UserService: errors detected");
+			return null;
+		}
+		
+		// all is good. continue & return user
+		return user;
 	}
 }
